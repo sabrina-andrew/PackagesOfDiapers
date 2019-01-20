6d5e6530f0483b1ee9787055d9658051d084aae5
Sabrina Andrew <sabrina.rae.andrew@gmail.com>
2019-01-20 13:58:14 -0500

    How Many Diapers Can You Buy

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HowManyDiapersCanYouBuy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //How many packages of diapers can you buy with \$40 if one package costs 8?
+
+            int diapers = 8;
+            int moneyInWallet = 40;
+            int packagesOfDiapers = moneyInWallet / diapers;
+            Console.WriteLine("You have enough money to buy " + packagesOfDiapers + " packages of diapers.");
+        }
+    }
+}
